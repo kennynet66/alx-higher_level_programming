923207f611b6b83f495c63ae9d86d1108c846a4e
Kennynet66 <kennynet66@gmail.com>
2023-12-05 17:48:16 +0300

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pyhon3
+""" A class MyList that inherits from list"""
+
+
+class MyList(list):
+    """A class MyList that has a public function that sorts in ascending order"""
+
+    def print_sorted(self):
+        """ Sorts a list in ascending order."""
+        print(sorted(self))
