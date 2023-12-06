227c3f935333f3cfe36784622b57d8b81db8b2cc
Kennynet66 <kennynet66@gmail.com>
2023-12-06 13:54:02 +0300

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,8 @@
     A function that reads a text file with the utf-8 encoding and prints it to the stdout
 """
 
+
 def read_file(filename=""):
+    """This function will open a file for reading"""
     with open(filename, encoding="utf-8") as myFile:
         print(myFile.read(), end="")
