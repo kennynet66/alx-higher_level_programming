8a31e9bd3e879ff900bc495f61db64a640fdb56b
Kenedy Maina <kennynet66@gmail.com>
2024-04-02 06:10:57 -0700

    fixed 15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
- -- script that lists the number of records with the same score in the table second_table of the database
+-- script that lists the number of records with the same score in the table second_table of the database
 SELECT score, COUNT(score) AS number
 FROM second_table
 GROUP BY score
